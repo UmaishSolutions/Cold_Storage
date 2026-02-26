1b7f12fd7837b814d7eb367f4eec06e94ce2c180
Umaish Solutions <solutions@umaish.com>
2026-02-26 23:03:06 +0500

    feat: Add new customer payment, billing, outstanding aging, and item movement reports.

---
+++ cold_storage/cold_storage/report/cold_storage_customer_outstanding_aging/cold_storage_customer_outstanding_aging.js
@@ -0,0 +1,45 @@
+frappe.query_reports["Cold Storage Customer Outstanding Aging"] = {
+	onload: async (query_report) => {
+		const company_filter = query_report.get_filter("company", false);
+		if (!company_filter) return;
+
+		try {
+			const configured_company = await frappe.db.get_single_value("Cold Storage Settings", "company");
+			if (configured_company) {
+				company_filter.df.default = configured_company;
+				company_filter.df.read_only = 1;
+				await company_filter.set_value(configured_company);
+			} else {
+				company_filter.df.read_only = 0;
+			}
+		} catch (error) {
+			console.warn("Unable to load Cold Storage Settings company for report filter", error);
+			company_filter.df.read_only = 0;
+		}
+
+		company_filter.refresh();
+	},
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			description: __("Filter by company."),
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+			description: __("Filter by customer."),
+		},
+		{
+			fieldname: "as_on_date",
+			label: __("As On Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			description: __("Filter by as on date."),
+		},
+	],
+};
